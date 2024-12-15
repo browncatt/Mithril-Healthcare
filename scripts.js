3c8711b59aef84b7ccedab844b383108efa0a09f
browncatt <gaellascott@gmail.com>
2024-12-15 20:04:00 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 // JavaScript for future interactivity or dynamic features
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Page Loaded Successfully');
+    
+    document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    // Toggle menu visibility
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
 });
